6dc48b0bd785fa0d23a09d8d91d8fc4ebcc1ce99
Nathan Nascimento <nathansantos3211@gmail.com>
2018-11-15 15:26:30 -0200

    no message

--- GerenciadorDeVeiculos/src/java/nathan/ads/gerenciadorDeVeiculos/models/VeiculoDao.java
+++ GerenciadorDeVeiculos/src/java/nathan/ads/gerenciadorDeVeiculos/models/VeiculoDao.java
@@ -1,11 +1,61 @@
 package nathan.ads.gerenciadorDeVeiculos.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Nathan
  */
 
 public class VeiculoDao {
-
     
+    private static List<Veiculo> veiculos = new ArrayList<>();
+    private static int sequenciaIds = 1;
+
+    public void inserir(Veiculo veiculo) {
+        
+        veiculo.setVeiculoId(sequenciaIds++);
+        veiculos.add(veiculo);
+    }
+
+    public Veiculo buscaPorId(long id) {
+        
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo.getVeiculoId()== id) {
+                return veiculo;
+            }
+        }
+        return null;
+    }
+
+    public void remover(long id) {
+        
+        Veiculo veiculo = buscaPorId(id);
+        if (veiculo != null) {
+            veiculos.remove(veiculo);
+        }
+    }
+
+    public void atualizar(Veiculo veiculoAtualizado) {
+        
+        remover(veiculoAtualizado.getVeiculoId());
+        veiculos.add(veiculoAtualizado);
+    }
+
+    public List<Veiculo> buscaTodos(String termoBusca) {
+        
+        if (termoBusca == null || termoBusca.isEmpty()) {
+            return veiculos;
+        }
+        List<Veiculo> produtosEncontrados = new ArrayList<>();
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo.getNomeVeiculo().contains(termoBusca)
+                    || veiculo.getDescricao().contains(termoBusca)) {
+                produtosEncontrados.add(veiculo);
+            }
+        }
+        
+        return produtosEncontrados;
+    }
     
 }
